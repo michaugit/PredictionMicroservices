8576fb8173a35a068b39bca8c6456f497c5e706b
Julia <juliaignacyk@student.agh.edu.pl>
2021-05-14 23:43:45 +0200

    nowe metody

--- PredictiveMicroservice/predictors.py
+++ PredictiveMicroservice/predictors.py
@@ -1,21 +1,21 @@
-import dataProvider
+import dataProvider, ThetaMethod, FFT
 import ExponentialSmoothing
 import matplotlib.pyplot as plt
 import numpy as np
 import mpld3
 import pandas as pd
-from darts import TimeSeries
+
 
 
 def tmp_predictor(to_predict, prediction_method):
     data = dataProvider.get_data(to_predict)
-    print(data)
-    series, train, val = prepare_data(data)
+    df = prepare_data(data['rates'])
     if prediction_method == 'metoda_1':
-        json_response = {'html_code': ExponentialSmoothing.predict(series, train, val)}
-        print(json_response)
+        json_response = {'html_code': ExponentialSmoothing.predict(df)}
     elif prediction_method == 'metoda_2':
-        print("XD")
+        json_response = {'html_code': ThetaMethod.predict(df)}
+    elif prediction_method == 'metoda_3':
+        json_response = {'html_code': FFT.predict(df)}
     else: print("kupa")
     print(prediction_method)
 
@@ -23,18 +23,11 @@ def tmp_predictor(to_predict, prediction_method):
 
 
 def prepare_data(datae):
-
-    df = pd.DataFrame.from_dict(datae['rates'])
-    print(df)
-    df.data = pd.to_datetime(df.data)
-    df = df.set_index('data')
-    df = df.reindex(pd.date_range(start=df.index[0], end=df.index[-1], freq='D'))
-    df = df.reindex(pd.date_range(start=df.index[0], end=df.index[-1], freq='D'))
-    df = df.fillna(method="pad", limit=5)
-    df = df.reset_index()
-    series = TimeSeries.from_dataframe(df, 'index', 'cena', 'D')
-    train, val = series.split_before(pd.Timestamp(df.at[33, 'index']))
-    return series, train, val
+    df = pd.DataFrame.from_dict(datae)
+    # print(df)
+    df.date = pd.to_datetime(df.date)
+    df.value = pd.to_numeric(df.value)
+    return df
 
 
 def tmp_matplotlib_html_generator():
